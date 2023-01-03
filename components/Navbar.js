63ddacb0cd474435f814470b73ecd515fcea8196
Eunice Oquialda <hi.euniceo@gmail.com>
2023-01-03 14:42:54 -0700

    added link to app name

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
     return ( 
         <nav>
             <div className="logo">
-                <Link href="/" passHref></Link><h1>Company Directory</h1>
+                <Link href="/" passHref><h1>Company Directory</h1></Link>
             </div>
             <Link href="/" passHref>Home</Link>
             <Link href="/about" passHref>About</Link>
